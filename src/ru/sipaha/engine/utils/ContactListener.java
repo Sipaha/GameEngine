457f830b1f647b8b654cdf4164718f5a4cc9abc9
Sipaha <Sipaha@bk.ru>
2014-11-04 02:55:24 +0700

    New animations and project structure

--- src/ru/sipaha/engine/utils/ContactListener.java
+++
@@ -1,8 +0,0 @@
-package ru.sipaha.engine.utils;
-
-import ru.sipaha.engine.core.GameObject;
-
-public interface ContactListener {
-    void beginContact(GameObject gameObject);
-    void endContact(GameObject gameObject);
-}
--- src/ru/sipaha/engine/utils/signals/Listener.java
+++
@@ -1,6 +0,0 @@
-package ru.sipaha.engine.utils.signals;
-
-public interface Listener<T> {
-
-	public void receive(T object);
-}
